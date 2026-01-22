03a9ec60054e36731c19130bad35f8b14bc4d89d
Azeru <rezadork91@gmail.com>
2026-01-23 01:33:25 +0700

    fix connfig.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,18 @@
 const config = {
-  // Deteksi environment otomatis
-  apiUrl:
-    import.meta.env.VITE_API_URL ||
-    (window.location.hostname === "localhost"
-      ? "http://localhost:8080/api"
-      : "https://https://uchida-be.onrender.com/api"), // Ganti dengan backend production URL
+  apiUrl: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
 };
 
+console.log("🔧 Config loaded:", config); // Debug
+
 export default config;
+
+// const config = {
+//   // Deteksi environment otomatis
+//   apiUrl:
+//     import.meta.env.VITE_API_URL ||
+//     (window.location.hostname === "localhost"
+//       ? "http://localhost:8080/api"
+//       : "https://https://uchida-be.onrender.com/api"), // Ganti dengan backend production URL
+// };
+
+// export default config;
